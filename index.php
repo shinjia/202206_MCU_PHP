010a0680319fde388bfbca01bcada9c646971d29
Shinjia Chen <shinjia168@gmail.com>
2022-07-28 22:16:10 +0800

    Class12

--- index.php
+++ index.php
@@ -9,79 +9,116 @@
 
 
 <div class="hot" style="background-color:rgb(235, 216, 189); padding:5px;">
-  <p>(快速連結) db_系列程式：|
-     <a href="db_mysqli/">db_mysqli</a> |
-     <a href="db_pdo_sth/">db_pdo_sth</a> |
-     <a href="db_ext/">db_ext</a> |
-     <a href="db_ext2/">db_ext2</a> |
-     <a href="db_pdo/">db_pdo</a> |
-     <a href="db2_pdo/">db2_pdo</a> ||| 
-	 <a href="my_memb/">my_memb</a> |
-     
-  </p>
+    <p>(快速連結) db_系列程式：|
+        <a href="db_mysqli/">db_mysqli</a> |
+        <a href="db_pdo_sth/">db_pdo_sth</a> |
+        <a href="db_ext/">db_ext</a> |
+        <a href="db_ext2/">db_ext2</a> |
+        <a href="db_pdo/">db_pdo</a> |
+        <a href="db2_pdo/">db2_pdo</a> ||| 
+        <a href="my_memb/">my_memb</a> |
+        
+    </p>
 </div>
 
-<h2>Class 11 (2022.07.26)</h2>
+
+
+
+<h2>Class 12 (2022/07/28)</h2>
+
+<p>預防網址 GET 被改掉 (php_uid_safe)</p>
+<ul>
+    <li><a href="php_uid_safe/">php_uid_safe</a></li>
+</li>
+</ul>
+
+<p>檔案上傳 (php_upload)</p>
+<ul>
+    <li><a href="php_upload/">php_upload</a></li>
+</ul>
+
+<p>前端網頁為主的上傳功能 (jQuery-File-Upload)</p>
+<ul>
+    <li><a href="jQuery-File-Upload/">jQuery-File-Upload</a></li>
+</ul>
+
+<p>PHP 處理 JSON (json_php)</p>
+<ul>
+    <li><a href="json_php/">json_php</a></li>
+</ul>
+
+<p>&nbsp;</p>
+
+
+
+
+
+<h2>Class 11 (2022/07/26)</h2>
 
 <p>Cookie (cookie)</p>
 <ul>
-  <li><a href="cookie/">cookie</a></li>
+    <li><a href="cookie/">cookie</a></li>
 </li>
 </ul>
 
 <p>db2_pdo_cookie (db2_pdo_cookie)</p>
 <ul>
-  <li><a href="db2_pdo_cookie/">db2_pdo_cookie</a></li>
+    <li><a href="db2_pdo_cookie/">db2_pdo_cookie</a></li>
 </ul>
 
 <p>session_login (session_login)</p>
 <ul>
-  <li><a href="session_login/">session_login</a></li>
+    <li><a href="session_login/">session_login</a></li>
 </ul>
 
 <p>&nbsp;</p>
 
 
 
-<h2>Class 10 (2022.07.21)</h2>
+
+
+<h2>Class 10 (2022/07/21)</h2>
 
 <p>db 加上 Bootstrap 風格</p>
 <ul>
-  <li><a href="db2_bootstrap/">db2_bootstrap</a></li>
+    <li><a href="db2_bootstrap/">db2_bootstrap</a></li>
 </ul>
 
 
 
-<h2>Class 09 (2022.07.19)</h2>
-<h2>Class 08 (2022.07.14)</h2>
-<h2>Class 07 (2022.07.12)</h2>
+
+
+<h2>Class 09 (2022/07/19)</h2>
+<h2>Class 08 (2022/07/14)</h2>
+<h2>Class 07 (2022/07/12)</h2>
 
 <p>參考上列資料庫系列程式</p>
 
 <p>補充：表單檢查的問題，參考 <a href="form_check_php/">form_check_php</a> 資料夾</p>
+
 <p>&nbsp;</p>
 
 
 
 
 
-<h2>Class 06 (2022.07.07)</h2>
+<h2>Class 06 (2022/07/07)</h2>
 
 <p>db_系列程式</p>
 <ul>
-  <li><a href="db_test/">db_test</a></li>
-  <li><a href="db_mysqli/">db_mysqli</a></li>
+    <li><a href="db_test/">db_test</a></li>
+    <li><a href="db_mysqli/">db_mysqli</a></li>
 </ul>
 
 <p>PHP 連結 MySQL 的程式測試 (db_test)</p>
 <ul>
-  <li>查看原始碼：[<a href="show_source.php?dir=db_test&amp;file=test1.php">db_test/test1.php</a>]</li>
+    <li>查看原始碼：[<a href="show_source.php?dir=db_test&amp;file=test1.php">db_test/test1.php</a>]</li>
 </li>
 </ul>
 
 <p>改出一套自己的資料庫練習程式</p>
 <ul>
-  <li><a href="my_book/">my_book 書籍資料管理</a></li>
+    <li><a href="my_book/">my_book 書籍資料管理</a></li>
 </ul>
 
 <p>&nbsp;</p>
@@ -90,39 +127,39 @@
 
 
 
-<h2>Class 05 (2022.07.05)</h2>
+<h2>Class 05 (2022/07/05)</h2>
 
 <p>陣列的用法</p>
 <ul>
-  <li>程式請見目錄內各檔案 <a href="array/">array</a></li>
+    <li>程式請見目錄內各檔案 <a href="array/">array</a></li>
 </ul>
 
 <p>六個幸運樂透數字 (lotto6)</p>
 <ul>
-  <li>-----複習------------------</li>
-  <li><a href="lotto6/lucky1.php">lucky1.php</a> [<a href="show_source.php?dir=lotto6&amp;file=lucky1.php">查看原始碼</a>] 回顧：產生一個隨機幸運球</li>
-  <li><a href="lotto6/lucky2.php">lucky2.php</a> [<a href="show_source.php?dir=lotto6&amp;file=lucky2.php">查看原始碼</a>] 回顧：產生多個隨機幸運球</li>
-  <li><a href="lotto6/lucky3.php">lucky3.php</a> 有趣的變化</li>
-  <li>-----程式的變化------------------</li>
-  <li><a href="lotto6/a1.php">a1.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a1.php">查看原始碼</a>] (產生六個球，有BUG，會發生重覆)</li>
-  <li><a href="lotto6/a2.php">a2.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a2.php">查看原始碼</a>] (六個球用陣列存放，有BUG，會發生重覆)</li>
-  <li><a href="lotto6/a3.php">a3.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a3.php">查看原始碼</a>] (不重覆) (含原來順序及排序後) (***主要的程式***)</li>
-  <li><a href="lotto6/a4.php">a4.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a4.php">查看原始碼</a>] (檢查陣列內是否有此數的程式寫法，功能同 in_array() 函式)</li>
-  <li><a href="lotto6/a5.php">a5.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a5.php">查看原始碼</a>] (上述的函式做成自訂函式，功能同 in_array() 函式)</li>  
-  <li>-----尚有其他演算法--------------</li>
-  <li><a href="lotto6/a6.php">a6.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a6.php">查看原始碼</a>] 演算法：全部的球放進盒子裡，隨機挑出六個</li>
-  <li><a href="lotto6/a7.php">a7.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a7.php">查看原始碼</a>] 演算法：全部的球放進盒子裡，打散，挑出前面六個</li>
+    <li>-----複習------------------</li>
+    <li><a href="lotto6/lucky1.php">lucky1.php</a> [<a href="show_source.php?dir=lotto6&amp;file=lucky1.php">查看原始碼</a>] 回顧：產生一個隨機幸運球</li>
+    <li><a href="lotto6/lucky2.php">lucky2.php</a> [<a href="show_source.php?dir=lotto6&amp;file=lucky2.php">查看原始碼</a>] 回顧：產生多個隨機幸運球</li>
+    <li><a href="lotto6/lucky3.php">lucky3.php</a> 有趣的變化</li>
+    <li>-----程式的變化------------------</li>
+    <li><a href="lotto6/a1.php">a1.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a1.php">查看原始碼</a>] (產生六個球，有BUG，會發生重覆)</li>
+    <li><a href="lotto6/a2.php">a2.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a2.php">查看原始碼</a>] (六個球用陣列存放，有BUG，會發生重覆)</li>
+    <li><a href="lotto6/a3.php">a3.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a3.php">查看原始碼</a>] (不重覆) (含原來順序及排序後) (***主要的程式***)</li>
+    <li><a href="lotto6/a4.php">a4.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a4.php">查看原始碼</a>] (檢查陣列內是否有此數的程式寫法，功能同 in_array() 函式)</li>
+    <li><a href="lotto6/a5.php">a5.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a5.php">查看原始碼</a>] (上述的函式做成自訂函式，功能同 in_array() 函式)</li>  
+    <li>-----尚有其他演算法--------------</li>
+    <li><a href="lotto6/a6.php">a6.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a6.php">查看原始碼</a>] 演算法：全部的球放進盒子裡，隨機挑出六個</li>
+    <li><a href="lotto6/a7.php">a7.php</a> [<a href="show_source.php?dir=lotto6&amp;file=a7.php">查看原始碼</a>] 演算法：全部的球放進盒子裡，打散，挑出前面六個</li>
 </ul>
-  
+
 <p>陣列用法補充 (array_sort) </p>
 <ul>
-  <li>程式請見目錄內各檔案 <a href="array_sort/">array_sort</a></li>
+    <li>程式請見目錄內各檔案 <a href="array_sort/">array_sort</a></li>
 </ul>
 
 
 <p>姓名產生器 (array) </p>
 <ul>
-  <li>簡易功能：<a href="array/name_generator.php">name_generator.php</a></li>
+    <li>簡易功能：<a href="array/name_generator.php">name_generator.php</a></li>
 </ul>
 
 <p>&nbsp;</p>
@@ -131,19 +168,19 @@
 
 
 
-<h2>Class 04 (2022.06.30)</h2>
+<h2>Class 04 (2022/06/30)</h2>
 
 <p>客戶意見留言 (comment)</p>
 <ul>
-  <li>程式執行 <a href="comment/input.php">input.php</a></li>
-  <li>查看原始碼：表單輸入 [<a href="show_source.php?dir=comment&amp;file=input.php">input.php</a>]</li>
-  <li>查看原始碼：結果存檔 [<a href="show_source.php?dir=comment&amp;file=save.php">save.php</a>]</li>
+    <li>程式執行 <a href="comment/input.php">input.php</a></li>
+    <li>查看原始碼：表單輸入 [<a href="show_source.php?dir=comment&amp;file=input.php">input.php</a>]</li>
+    <li>查看原始碼：結果存檔 [<a href="show_source.php?dir=comment&amp;file=save.php">save.php</a>]</li>
 </ul>
-  
+
 <p>線上問卷調查 (survey)</p>
 <ul>
-  <li>v1 程式執行 <a href="survey/v1/input.php">input.php</a> </li>
-  <li>v2 程式執行 <a href="survey/v2/input.php">input.php</a> </li>
+    <li>v1 程式執行 <a href="survey/v1/input.php">input.php</a> </li>
+    <li>v2 程式執行 <a href="survey/v2/input.php">input.php</a> </li>
 </ul>
 
 <p>&nbsp;</p>
@@ -152,65 +189,65 @@
 
 
 
-<h2>Class 03 (2022.06.28)</h2>
+<h2>Class 03 (2022/06/28)</h2>
 
 <p>BMI身體質量指數 (bmi)</p>
 <ul>
-  <li>程式執行 <a href="bmi/input.php">input.php</a> </li>
-  <li>查看原始碼：表單輸入 [<a href="show_source.php?dir=bmi&amp;file=input.php">input.php</a>]</li>
-  <li>查看原始碼：計算結果 [<a href="show_source.php?dir=bmi&amp;file=calc.php">calc.php</a>]</li>
+    <li>程式執行 <a href="bmi/input.php">input.php</a> </li>
+    <li>查看原始碼：表單輸入 [<a href="show_source.php?dir=bmi&amp;file=input.php">input.php</a>]</li>
+    <li>查看原始碼：計算結果 [<a href="show_source.php?dir=bmi&amp;file=calc.php">calc.php</a>]</li>
 </ul>
 <p>BMI身體質量指數 (bmi_check)</p>
 <ul>
-  <li>說明：增加輸入資料檢查的 Javascript</li>
-  <li>程式執行 <a href="bmi_check/input.php">input.php</a> </li>
-  <li>查看原始碼：表單輸入 [<a href="show_source.php?dir=bmi_check&amp;file=input.php">input.php</a>]</li>
-  <li>查看原始碼：計算結果 [<a href="show_source.php?dir=bmi_check&amp;file=calc.php">calc.php</a>]</li>
+    <li>說明：增加輸入資料檢查的 Javascript</li>
+    <li>程式執行 <a href="bmi_check/input.php">input.php</a> </li>
+    <li>查看原始碼：表單輸入 [<a href="show_source.php?dir=bmi_check&amp;file=input.php">input.php</a>]</li>
+    <li>查看原始碼：計算結果 [<a href="show_source.php?dir=bmi_check&amp;file=calc.php">calc.php</a>]</li>
 </ul>
 <p>JavaScript 表單檢查 (form_check)</p>
 <ul>
-  <li><a href="form_check/check1.html">check1.html</a> </li>
-  <li><a href="form_check/check2.html">check2.html</a> </li>
-  <li><a href="form_check/check3.html">check3.html</a> </li>
-  <li><a href="form_check/summary_js.html">summary_js.html 各種元件的檢查寫法</a> </li>
+    <li><a href="form_check/check1.html">check1.html</a> </li>
+    <li><a href="form_check/check2.html">check2.html</a> </li>
+    <li><a href="form_check/check3.html">check3.html</a> </li>
+    <li><a href="form_check/summary_js.html">summary_js.html 各種元件的檢查寫法</a> </li>
 </ul>
 
 <p>&nbsp;</p>
 
-  
 
 
 
-<h2>Class 02 (2022.06.23)</h2>
+
+<h2>Class 02 (2022/06/23)</h2>
 
 <p>九九乘法練習 (game99)</p>
 <ul>
-  <li>練習圖檔下載：<a href="game99/images_game99.zip">images_game99.zip</a></li>
-  <li>程式執行 <a href="game99/question.php">question.php</a> </li>
-  <li>查看原始碼：出題 [<a href="show_source.php?dir=game99&amp;file=question.php">question.php</a>]</li>
-  <li>查看原始碼：顯示答案 [<a href="show_source.php?dir=game99&amp;file=answer.php">answer.php</a>]</li>
+    <li>練習圖檔下載：<a href="game99/images_game99.zip">images_game99.zip</a></li>
+    <li>程式執行 <a href="game99/question.php">question.php</a> </li>
+    <li>查看原始碼：出題 [<a href="show_source.php?dir=game99&amp;file=question.php">question.php</a>]</li>
+    <li>查看原始碼：顯示答案 [<a href="show_source.php?dir=game99&amp;file=answer.php">answer.php</a>]</li>
 </ul>
 
 <p>在網頁上也會出現 GET 傳遞資料用法</p>
 <ul>
-  <li><a href="https://www.google.com.tw/search?q=php" target="_blank">查看更多『php』的資料</a></li>
-  <li><a href="https://www.google.com.tw/search?q=程式" target="_blank">查看更多『程式』的資料</a></li>
-  <a href="https://www.google.com/search?q=捍衛戰士" target="_blank">查詢更多『捍衛戰士』</a>
+    <li><a href="https://www.google.com.tw/search?q=php" target="_blank">查看更多『php』的資料</a></li>
+    <li><a href="https://www.google.com.tw/search?q=程式" target="_blank">查看更多『程式』的資料</a></li>
+    <a href="https://www.google.com/search?q=捍衛戰士" target="_blank">查詢更多『捍衛戰士』</a>
 </ul>
 
 <p>用表單(GET)傳遞資料用法</p>
 <ul>
-  <li>
-    <form method="get" action="https://www.google.com/search">
-      <p>查詢：<input type="text" name="q" value="PHP">
-      <input type="submit" value="送出"></p>
-    </form>
-  </li>
-  <li>上述範例請參考原始碼</li>
+    <li>
+        <form method="get" action="https://www.google.com/search">
+        <p>查詢：<input type="text" name="q" value="PHP">
+        <input type="submit" value="送出"></p>
+        </form>
+    </li>
+    <li>上述範例請參考原始碼</li>
 </ul>
 <p>上週作業解答 (rand_pic)</p>
 <ul>
-  <li>程式執行 <a href="rand_pic/index.php">rand_pic/index.php</a></li>
+    <li>程式執行 <a href="rand_pic/index.php">rand_pic/index.php</a></li>
 </ul>
 
 <p>&nbsp;</p>
@@ -219,17 +256,17 @@
 
 
 
-<h2>Class 01 (2022.06.21)</h2>
+<h2>Class 01 (2022/06/21)</h2>
 
 <p>自我介紹 (intro)</p>
 <ul>
-  <li>第一個php程式 (嵌入式) <a href="intro/test.php">test.php</a> [<a href="show_source.php?dir=intro&amp;file=test.php">查看原始碼</a>]</li>
-  <li>程式與網頁分離的概念 <a href="intro/intro.php">intro.php</a> [<a href="show_source.php?dir=intro&amp;file=intro.php">查看原始碼</a>]</li>
+    <li>第一個php程式 (嵌入式) <a href="intro/test.php">test.php</a> [<a href="show_source.php?dir=intro&amp;file=test.php">查看原始碼</a>]</li>
+    <li>程式與網頁分離的概念 <a href="intro/intro.php">intro.php</a> [<a href="show_source.php?dir=intro&amp;file=intro.php">查看原始碼</a>]</li>
 </ul>
 
 <p>幸運數字 (lucky)</p>
 <ul>
-  <li>Lucky Number <a href="lucky/index.php">index.php</a> [<a href="show_source.php?dir=lucky&amp;file=index.php">查看原始碼</a>]</li>
+    <li>Lucky Number <a href="lucky/index.php">index.php</a> [<a href="show_source.php?dir=lucky&amp;file=index.php">查看原始碼</a>]</li>
 </ul>
 
 <p>&nbsp;</p>
